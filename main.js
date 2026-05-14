8820cca04e3c51d853c5cf9e8c506eddb2c54748
JJonKang <jjonkang03@gmail.com>
2026-05-14 17:06:07 -0500

    Initialized Project

---
+++ main.js
@@ -0,0 +1,7 @@
+// Project: Ripple Effect using GLSL
+// Author: Jonathan Kang
+// Main Focus: Creating a fragment shader that allows
+//             a ripple effect on surfaces.
+
+import vertexShaderSrc from './vertex.glsl.js';
+import fragmentShaderSrc from './fragment.glsl.js';
