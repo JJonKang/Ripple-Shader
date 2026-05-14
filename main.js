667fb314fdd7d25316d93e5a934a0d745d9a4e89
JJonKang <jjonkang03@gmail.com>
2026-05-14 18:03:27 -0500

    Implemented webgl boilerplate

--- main.js
+++ main.js
@@ -5,3 +5,74 @@
 
 import vertexShaderSrc from './vertex.glsl.js';
 import fragmentShaderSrc from './fragment.glsl.js';
+
+/////////////////////////////////////////////////////
+// Create the Program
+function createProgram(gl, vshader, fshader){
+  const program = gl.createProgram();
+  gl.attachShader(program, vshader);
+  gl.attachShader(program, fshader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+  if(!gl.getProgramParameter(program, gl.LINK_STATUS) ) {
+    let info = gl.getProgramInfoLog(program);
+    console.log('Could not compile WebGL program:' + info);
+  }
+
+  return program;
+};
+
+/////////////////////////////////////////////////////
+// Create Shader (vertex and fragment)
+function createShader(gl, type, source){
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+    let info = gl.getShaderInfoLog(shader);
+    console.log('Could not compile WebGL program:' + info);
+  }
+  return shader;
+};
+
+/////////////////////////////////////////////////////
+// Initialization and Rendering
+function initialize(){
+  // screen setup
+  const canvas = document.querySelector('canvas');
+  canvas.width = canvas.clientWidth * 3.0;
+  canvas.height = canvas.clientHeight * 3.0;
+  const gl = canvas.getContext('webgl2');
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+
+  // create shaders
+  const vshader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSrc);
+  const fshader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc);
+
+  // create and link program
+  const program = createProgram(gl, vshader, fshader);
+
+  // create vertices buffers
+  const buf = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,-1, 1,-1, -1,1, -1,1, 1,-1, 1,1]), gl.STATIC_DRAW);
+  const posLoc = gl.getAttribLocation(program, 'position');
+  gl.enableVertexAttribArray(posLoc);
+  gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 0, 0);
+
+  const uRes = gl.getUniformLocation(program, 'u_resolution');
+  const uTime = gl.getUniformLocation(program, 'u_time');
+  gl.uniform2f(uRes, canvas.width, canvas.height);
+
+  function render(t) {
+    gl.uniform1f(uTime, t * 0.001);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    requestAnimationFrame(render);
+  };
+
+  requestAnimationFrame(render);
+};
+
+window.onload = initialize;
