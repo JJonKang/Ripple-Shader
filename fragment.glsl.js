ce96db6a8c40098caf56a007d6ebaede69804fd4
JJonKang <jjonkang03@gmail.com>
2026-05-18 23:30:57 -0500

    Added some comments

--- fragment.glsl.js
+++ fragment.glsl.js
@@ -28,8 +28,8 @@ float smoothQuintic(float a, float b, float c) {
 }
 
 float noise(vec2 p) {
-  vec2 i = floor(p);
-  vec2 f = fract(p);
+  vec2 i = floor(p); //current cell
+  vec2 f = fract(p); //within curr cell
   vec2 u = f * f * f * (f * (f * 6.0 - 15.0) + 10.0);
   return mix(
     mix(random1(i),             random1(i + vec2(1,0)), u.x),
