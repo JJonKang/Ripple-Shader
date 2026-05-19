22c17e143022e075acd8bd5dbf71e0bf4317d349
JJonKang <jjonkang03@gmail.com>
2026-05-18 20:42:16 -0500

    Small number adjustments for better lighting and ripple effects

--- fragment.glsl.js
+++ fragment.glsl.js
@@ -4,7 +4,7 @@ export default `#version 300 es
 precision mediump float;
 
 #define CELL 2
-#define numOctaves 2
+#define numOctaves 1
 #define HASHSCALE1 .1031
 #define HASHSCALE3 vec3(.1031, .1030, .0973)
 
@@ -67,30 +67,30 @@ float pattern( in vec2 p ){
 
 void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution.y * 6.0;
-  uv += pattern(uv + u_time * 0.1) * 0.15;
+  uv += pattern(uv + u_time * 0.1) * 0.105;
   vec2 p0 = floor(uv);  
   vec2 circles = vec2(0.0, 0.0);
   for (int y = -CELL; y <= CELL; y++){
     for (int x = -CELL; x <= CELL; x++){
-      vec2 pi = p0 + vec2(x, y);         // neighbor cell
-      vec2 p = pi + hash22(pi);           // random drop position in cell
+      vec2 pi = p0 + vec2(x, y); // neighbor cell
+      vec2 p = pi + hash22(pi); // random pos ripple start in a cell
       float t = fract(0.3 * u_time + hash12(pi)); // random lifecycle 0->1
 
-      vec2 v = p - uv;                    // vector from drop to pixel
+      vec2 v = p - uv; // vector from drop to pixel
       float d = length(v) - float(CELL + 1) * t; // ring SDF
 
       float h = 1e-3;
       float d1 = d - h;
       float d2 = d + h;
-      float p1 = sin(31.0 * d1) * smoothQuintic(-0.6, -0.3, d1) * smoothQuintic(0.0, -0.3, d1);
-      float p2 = sin(31.0 * d2) * smoothQuintic(-0.6, -0.3, d2) * smoothQuintic(0.0, -0.3, d2);
+      float p1 = sin(25.0 * d1) * smoothQuintic(-0.6, -0.3, d1) * smoothQuintic(0.01  , -0.3, d1);
+      float p2 = sin(25.0 * d2) * smoothQuintic(-0.6, -0.3, d2) * smoothQuintic(0.01, -0.3, d2);
       circles += 0.5 * normalize(v) * ((p2 - p1) / (2.0 * h) * (1.0 - t) * (1.0 - t));
     }
   }
   circles /= float((CELL*2+1)*(CELL*2+1));
   float z = 1.0 - dot(circles, circles);
   vec3 n = vec3(circles, sqrt(max(0.0, z)));
-  float wave = dot(n, normalize(vec3(1.0, 0.7, 0.5)));
+  float wave = dot(n, normalize(vec3(0.9, 0.75, 0.45)));
   fragColor = vec4(vec3(wave), 1.0);
 }
 `;
